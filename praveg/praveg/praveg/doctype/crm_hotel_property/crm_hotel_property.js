3184c3fba72bfde8ce72125897365acfa18822a8
Nilesh Vlanki <info@shayona.biz>
2026-02-18 19:43:34 +0530

    your message

--- praveg/praveg/praveg/doctype/crm_hotel_property/crm_hotel_property.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Yash Solanki - Shayona and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("CRM Hotel Property", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- praveg/praveg/praveg/doctype/crm_meal_plan/crm_meal_plan.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Yash Solanki - Shayona and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class CRMMealPlan(Document):
-	pass
